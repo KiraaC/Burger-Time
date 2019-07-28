aaaf3c246a1d6d7d9a5f06c002f4a6dfcbb11984
Kira Corbin <madkatkeys@gmail.com>
2019-07-28 13:34:43 -0700

    added orm.js & connection.js

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+const connection = require("../config/connection.js");
+
+let orm = {
+  selectWhere: function(tableInput, colToSearch, valOfCol) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      if (err) throw err;
+      return result;
+    });
+  }
+};
+
+module.exports = orm;
