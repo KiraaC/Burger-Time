29b51a3a31e8836566bd5b266bf54373faacfcec
Kira Corbin <madkatkeys@gmail.com>
2019-07-28 14:01:45 -0700

    added burgers__controllers.js

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,3 +1,37 @@
-const 
-const
-const
+const express = require('express');
+const burger = require('../models/burger');
+
+const router = express.Router();
+console.log("launch controller");
+
+// get all
+router.get('/', function (req, res) {
+    // console.log('')
+    burger.selectAll(function (data) {
+        // console.log('')
+        res.render('index', { burgerObj: data });
+    });
+});
+
+// insert
+router.post('/api/newBurger', function (req, res) {
+    // console.log('')
+    burger.insertOne(['burger_name'], [req.body.burger_name], function (result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+// update
+router.put('/api/devorIt/:id', function (req, res) {
+    // console.log('')
+    burger.updateOne(req.params.id, function (results) {
+        if (results.changedRows == 0) {
+            return res.status(404).end();
+        } elese {
+            res.status(200).end();
+        }
+    });
+});
+
+
+module.exports = router;
