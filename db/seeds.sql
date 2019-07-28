5b853bead254904c98c0fa32ce50174389113318
Kira Corbin <madkatkeys@gmail.com>
2019-07-27 22:58:32 -0700

    worked on mysql seeds and schema

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name,  devoured) VALUES (1,'Avocado Burger');
+INSERT INTO burgers (burger_name,  devoured) VALUES (2,'Salmon Burger');
+INSERT INTO burgers (burger_name,  devoured) VALUES (3,'Tofu Burger');
+INSERT INTO burgers (burger_name,  devoured) VALUES (4,'Breakfast Burger');
+INSERT INTO burgers (burger_name,  devoured) VALUES (5,'Western Burger');
+
+-- INSERT INTO menu (item, entree) VALUES ('Western Burger');
+-- INSERT INTO menu (item, entree) VALUES ('Veggie Burger');
