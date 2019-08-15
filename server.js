7ff52c70a665e4b5db591c052df946af85a3261d
Kira Corbin <madkatkeys@gmail.com>
2019-08-15 15:36:25 -0700

    uncommented a parser line in code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphds = require('express-handlebars');
 
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
