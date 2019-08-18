4a3b9269a3108f4a144bc27580ec3c48fa3ab9e2
Kira Corbin <madkatkeys@gmail.com>
2019-08-18 14:57:03 -0700

    added and modified lines of code in orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,33 +1,57 @@
 const connection = require("../config/connection");
 
 let orm = {
-  selectWhere: function (tableInput, colToSearch, valOfCol) {
-    const queryString = "SELECT * FROM ?? WHERE ?? = ?";
+  selectAll: function (tableInput, cb) {
+    let queryString = "SELECT * FROM " + tableInput + ";";
 
-    connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
-      if (err) throw err;
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
       return result;
     });
   },
 
   create: function (table, cols, vals, cb) {
-    queryString = "INSERT INTO " + table + "(" + cols + ")" + "VALUES (" + printQuestionMarks(vals.length) + ")"
-    console.log('queryString');
+    let queryString = "INSERT INTO " + table
+    queryString = "(" + cols.toString() + ")" + "VALUES (" + printQuestionMarks(vals.length) + ")";
+
+    console.log(queryString);
+
     connection.query(queryString, vals, function (err, res) {
-      if (err) throw err;
-      else cb(res) 
-    }
-    )
+
+      if (err) {
+        throw err;
+      }
+      cb(res)
+    });
+  },
+  update: function (table, objectColVals, condition, cb) {
+    let queryString = "UPDATE " + table;
+    queryString = " SET " + objectToSql(objectColVals) + " WHERE " + condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
   }
 };
-
 printQuestionMarks = function (num) {
-  let array = []
-
+  let array = [];
   for (let i = 0; i < num; i++) {
     array.push("?")
   }
   return array.toString()
 }
 
+function objectToSql(ob) {
+  var arr = [];
+  for (var key in ob) {
+    arr.push(key + "=" + ob[key]);
+  }
+  return arr.toString();
+}
 module.exports = orm;
